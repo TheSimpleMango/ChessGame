08a9cc9eb15ca17891107b3642cfc3d425e84fdc
Joel Forman <league@WTS11.attlocal.net>
2016-08-28 14:10:25 -0700

    asdf

--- Java/src/Board.java
+++ Java/src/Board.java
@@ -1,4 +1,20 @@
+import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class Board {
 
+	public ArrayList<Tile> tiles = new ArrayList<Tile>();
+	
+	public Board(){
+		
+	}
+
+	public void update(){
+		
+	}
+	
+	public void draw(Graphics g){
+		
+	}
+	
 }
--- Java/src/Tile.java
+++ Java/src/Tile.java
@@ -1,4 +1,18 @@
 
 public class Tile {
 
+	private boolean isOccupied;
+	
+	public Tile(){
+		
+	}
+	
+	public boolean getOccupation(){
+		return isOccupied;
+	}
+	
+	public void setOccupation(boolean b){
+		isOccupied = true;
+	}
+	
 }
