c7fcdbe306a4855ba3164097b43e7048ad1d2165
Charlie <league@WTS10.attlocal.net>
2016-08-28 14:20:07 -0700

    Added turns and colors
    If color is black, selects them to go first. If color is white, sets
    turn to false.

--- Java/src/Player.java
+++ Java/src/Player.java
@@ -1,13 +1,29 @@
+import java.awt.Color;
 import java.util.ArrayList;
 
 public class Player {
 	String name;
+	Color color;
+	boolean turn; // True if it is the player's turn. False otherwise
 	public ArrayList<Piece> pieces = new ArrayList<Piece>();
 	// you may change the whole arraylist by using setPieces or just use
 	// (playername).pieces.....
 
-	Player(String name) {
+	Player(String name, Color color) {
 		this.name = name;
+		this.color = color;
+
+	}
+
+	Player(String name, String color) {
+		this.name = name;
+		if (color.equalsIgnoreCase("black")) {
+			this.color = Color.black;
+			turn = true;
+		} else if (color.equalsIgnoreCase("white")) {
+			this.color = Color.white;
+			turn = false;
+		}
 	}
 
 	public String getName() {
@@ -29,4 +45,18 @@ public class Player {
 	public void addPiece(Piece p) {
 		pieces.add(p);
 	}
+
+	public boolean toggleTurn() {
+		turn = !turn;
+		return turn;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
 }
