a698bbcb825fa290c218a06a9bcfa05d21fed46a
Charlie <league@WTS10.attlocal.net>
2016-08-28 13:57:10 -0700

    add Pieces arraylist

--- Java/src/Player.java
+++ Java/src/Player.java
@@ -1,12 +1,22 @@
+import java.util.ArrayList;
 
 public class Player {
 	String name;
-
+	public ArrayList<Piece> pieces = new ArrayList<Piece>();
+	//you may change the whole arraylist by using setPieces or just use (playername).pieces.....
 	Player(String name) {
 		this.name = name;
 	}
 
 	public String getName() {
 		return name;
+	}
+
+	public ArrayList<Piece> getPieces() {
+		return pieces;
+	}
+
+	public void setPieces(ArrayList<Piece> pieces) {
+		this.pieces = pieces;
 	}
 }
