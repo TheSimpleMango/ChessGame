0983f657b1aba8814b1438e706f616458ec78c0c
Joel Forman <league@WTS11.attlocal.net>
2016-09-04 14:13:25 -0700

    added tiles

--- Java/src/GUI.java
+++ Java/src/GUI.java
@@ -2,23 +2,27 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class GUI implements MouseListener {
-	JFrame f = new JFrame("Chess");
+public class GUI extends JFrame implements MouseListener, ActionListener{
 	JPanel p = new JPanel(new GridLayout(8, 8));
-	JLabel bL = new JLabel();
+	JButton b11 = new JButton();
 	Board b = new Board();
+	Timer t = new Timer(100, this);
+	Boolean hasPiece = false;
 	
 	public GUI() {
-		//add board
-		f.add(p);
-		f.pack();
+		this.add(p);
+		this.pack();
 		p.addMouseListener(this);
 	}
 
@@ -29,24 +33,30 @@ public class GUI implements MouseListener {
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		Tile t = (Tile) e.getSource();
+		Button t = (Button) e.getSource();
 		if(t.getOccupation() == true){
-			
+			hasPiece = true;
 		}
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
+		
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		repaint();
+	}
+	
 	
 	
 }
