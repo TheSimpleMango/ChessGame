d6a77584f035c688c6901c2e41015099dcc8c672
rylr <league@WTS5.attlocal.net>
2016-09-11 13:16:27 -0700

    fixed main method

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
+import java.io.IOException;
+
 import javax.swing.JOptionPane;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Player black = new Player(JOptionPane.showInputDialog("Please enter name of player 1"));
 		Player white = new Player(JOptionPane.showInputDialog("Please enter name of player 2"));
-=======
-import java.io.IOException;
-
-public class Main {
-	public static void main(String[] args) throws IOException {
->>>>>>> TheSimpleMango/master
 		GUI g = new GUI();
 	}
 }
